c999f791337e36ba5550dfd6bd02d24fbcf2182f
Tom Rooker <tomdrooker@gmail.com>
2018-04-28 22:54:35 +0100

    First commit.

---
+++ src/util/Yelp.js
@@ -0,0 +1,29 @@
+const apiKey = '-ereJz_smRnlRJtEHAA3z-CGWJWWDECTQ1KuTeYqypNPHbpmGI_v8m4tibIxWCHQcl0PCNyyNlPkLvnnRGZnPjgBe4LCPa5Yua-XZG3hcggMJF-LrV3DHCt-h8rkWnYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => response.json()).then(jsonResponse => {
+    if (jsonResponse.businesses) {
+      jsonResponse.businesses.map(business => {
+         return {
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          postCode: business.post_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }
+      });
+    }
+  });
+  }
+};
+
+export default Yelp;
